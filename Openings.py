66fe3412bff0a4ab7e7903d723ad79f53d614e57
jasonchen2023 <77142116+jasonchen2023@users.noreply.github.com>
2022-02-10 17:51:30 -0800

    Add files via upload

---
+++ Openings.py
@@ -0,0 +1,17 @@
+Italian = ["e2e4", "e7e5", "g1f3", "b8c6", "f1c4"]
+Sicilian_Defense = ["e2e4", "c7c5"]
+French_Defense = ["e2e4", "e7e6", "d2d4", "d7d5"]
+Ruy_Lopez = ["e2e4", "e7e5", "g1f3", "b8c6", "f1b5"]
+Slav_Defense = ["d2d4", "d7d5", "c2c4", "c7c6"]
+
+
+openings = [Italian, Sicilian_Defense, French_Defense, Ruy_Lopez, Slav_Defense]
+
+
+openings_dict = {
+    0: "Italian",
+    1: "Sicilian Defense",
+    2: "French Defense",
+    3: "Ruy-Lopez",
+    4: "Slav Defense"
+}
